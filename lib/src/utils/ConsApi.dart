1883131af22c2d2bd3ca7101fd9905d2581fae3c
Johnny <johnnygar98@hotmail.com>
2019-12-06 16:56:00 -0500

    Configurando el servidor

--- lib/src/utils/ConsApi.dart
+++ lib/src/utils/ConsApi.dart
@@ -1,8 +1,8 @@
 class ConsApi {
 
-  //static String path = "http://34.69.91.44/api/";
+  static String path = "http://35.202.33.238/api/";
 
-  static String path = "http://192.168.1.10/pera-public/api/";
+  //static String path = "http://192.168.1.10/pera-public/api/";
   //static String path = "http://192.168.137.126/pera-public/api/";
 
 }
