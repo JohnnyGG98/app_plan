8cddccfae221bdb4575b2b9287f8a2ca50495442
Gustavo <johnnygar98@hotmail.com>
2019-12-07 14:28:05 -0500

    Script

--- lib/src/providers/sqlite/BD.dart
+++ lib/src/providers/sqlite/BD.dart
@@ -29,33 +29,34 @@ class BD {
       onOpen: (db) {},
       onCreate:  (Database db, int version ) async {
         await db.execute(
-          'CREATE TABLE cursoasistencia ('
-          ' id_curso INTEGER, '
-          ' periodo TEXT, '
-          ' materia TEXT, '
-          ' curso TEXT, '
-          ' dia INTEGER, '
-          ' horas INTEGER '
-          '); '
-          'CREATE TABLE alumnocursoasistencia( '
-          ' id_curso INTEGER, '
-          ' id_almn_curso INTEGER, '
-          ' alumno TEXT '
-          '); '
-          'CREATE TABLE asistenciaoffline ( '
-          ' id INTEGER PRIMARY KEY, '
-          ' id_curso INTEGER, '
-          ' id_almn_curso INTEGER, '
-          ' alumno TEXT, '
-          ' horas INTEGER, '
-          ' fecha TEXT '
-          '); '
-          'CREATE TABLE fechasclase ( '
-          ' id_curso INTEGER, '
-          ' fecha TEXT, '
-          ' dia INTEGER, '
-          ' horas INTEGER '
-          '); '
+          '''
+          CREATE TABLE cursoasistencia (
+           id_curso INTEGER, 
+           periodo TEXT, 
+           materia TEXT, 
+           curso TEXT, 
+           dia INTEGER, 
+           horas INTEGER 
+          ); 
+          CREATE TABLE alumnocursoasistencia( 
+           id_curso INTEGER, 
+           id_almn_curso INTEGER, 
+           alumno TEXT '
+          ); '
+          CREATE TABLE asistenciaoffline ( 
+           id INTEGER PRIMARY KEY, 
+           id_curso INTEGER, 
+           id_almn_curso INTEGER, 
+           alumno TEXT, 
+           horas INTEGER, 
+           fecha TEXT 
+          ); 
+          CREATE TABLE fechasclase ( 
+           id_curso INTEGER, 
+           fecha TEXT, 
+           dia INTEGER, 
+           horas INTEGER
+          ); '''
         );
 
         print('CREADA');
